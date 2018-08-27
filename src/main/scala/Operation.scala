1e2610575d4c6bd6a515a77763eb8c92e5647328
Tekunov Daniil <ditekunov@yandex.ru>
2018-08-27 10:27:41 +0300

    Update Operation.scala

--- src/main/scala/Operation.scala
+++ src/main/scala/Operation.scala
@@ -2,23 +2,40 @@ package oper
 sealed trait Operation
 
 sealed trait QueuedOperation {
-  val name : String
-  val currency : String
-  val count : Int
-  val price : Double
+  val name: String
+  val currency: String
+  val count: Int
+  val price: Double
 }
-case class Purchase(name : String, currency : String, count : Int, price : Double) extends QueuedOperation
-case class Selling(name : String, currency : String, count : Int, price : Double) extends QueuedOperation
 
-case class CurrencyAddition(currency : String, startCount : Int = 0)
-case class CurrencyRemoval(currency : String, price : Double = 0)
+/**
+  * Case class that handles purchasing operations
+  */ 
+case class Purchase(name: String,
+                    currency: String,
+                    count: Int,
+                    price: Double) extends QueuedOperation
 
-case class ClientRemoval(name : String)
+/**
+  * Case class that handles selling operations
+  */ 
+case class Selling(name: String,
+                   currency: String,
+                   count: Int,
+                   price: Double) extends QueuedOperation
 
-case class ChangingClientBalance(name : String, balance: Double = 0)
-case class ChangingClientCurrency(name : String, currency: String, count : Int)
+
+case class CurrencyAddition(currency: String, startCount: Int = 0)
+
+case class CurrencyRemoval(currency: String, price: Double = 0)
+
+case class ClientRemoval(name: String)
+
+case class ChangingClientBalance(name: String, balance: Double = 0)
+case class ChangingClientCurrency(name: String, currency: String, count : Int)
 
 object OperationParser{
-  def parseLine(line : String, lineParser: String => QueuedOperation) : QueuedOperation = lineParser(line)
-  def parseLines(lines : List[String], lineParser: String => QueuedOperation) : List[QueuedOperation] = lines.map(lineParser)
+  def parseLine(line: String, lineParser: String => QueuedOperation) : QueuedOperation = lineParser(line)
+  def parseLines(lines: List[String], lineParser: String => QueuedOperation) : List[QueuedOperation] = lines.map(lineParser)
+  
 }
